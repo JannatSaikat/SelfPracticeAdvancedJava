4dc14ec266c4961a1002631a4c35cff9db1806f3
Jannat Saikat <jannatsaikat@gmail.com>
2020-08-18 00:52:07 -0400

    lambda expression

--- src/lambdaexpression/anonymous/OperateLaptop.java
+++ src/lambdaexpression/anonymous/OperateLaptop.java
@@ -4,16 +4,23 @@ public class OperateLaptop {
 
     public static void main(String[] args) {
         //Please implement anonymous class for interface laptop
-        Laptop laptop = null;
+        Laptop laptop = new Laptop() {
+            @Override
+            public void ramSize(int memorySize) {
+                System.out.println("RAM size is: " + memorySize + " GB");
+            }
 
+            @Override
+            public void monitorSize(int screenSize) {
+                System.out.println("Monitor size is: " + screenSize);
+            }
+        };
 
-
-
-        laptop.monitorSize(16);
+        laptop.ramSize(16);
         laptop.monitorSize(15);
 
         //please implement anonymous function for Phone FunctionalInterface
-
-
+        Phone phone = network -> System.out.println("New Network: " + network);
+        phone.cellularNetwork("5G");
     }
 }
